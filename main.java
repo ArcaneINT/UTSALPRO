ea6d1fbfedf24ad1a04114a2b71f132bddf5e756
ArcaneINT <115127431+ArcaneINT@users.noreply.github.com>
2022-11-24 10:56:32 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main
+{
+    public static void main(String[] args) {
+        String nama = "Muhammad Fauzan Arkan";
+        int umur = 2002;
+        
+        System.out.println("Assalamualaikum");
+        System.out.println("Nama saya :"+ nama);
+        System.out.println("Saya lahir tahun :" + umur);
+    }
+}
